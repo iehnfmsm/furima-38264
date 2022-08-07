b435942aa371fbab696452b7ca4cfe217c688d40
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-07 17:41:25 +0900

    単体テストコード正常系

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname {Faker::Name.name}
-    email {Faker::Internet.free_mail}
+    email {Faker::Internet.free_email}
     password {Faker::Internet.password(min_length:6)}
     password_confirmation {password}
     japanese_user = Gimei.name
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,16 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
 
-  
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "すべての項目が記入されている" do
+        expect(@user).to be_valid
+      end
+    end
+    context '新規登録できない場合' do
+      it "" do
+      end
+    end
+
+  end
 end
