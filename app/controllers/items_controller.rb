181bc5a11cca67dbe2b87dd789fb1dbc70f611b7
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-08 16:34:30 +0900

    商品出品ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
 
   end
@@ -19,7 +21,7 @@ class ItemsController < ApplicationController
 private
 
 def item_params
-  params.require(:item).permit(:title, :introduction, :price, :category_id, :status_id, :prefecture_id, :delivery_charge_id, :delivery_date_id).merge(user_id: current_user.id)
+  params.require(:item).permit(:title, :introduction, :price, :category_id, :status_id, :prefecture_id, :delivery_charge_id, :delivery_date_id, :image).merge(user_id: current_user.id)
 end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
 
   validates :title, presence: true
   validates :introduction, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"}
