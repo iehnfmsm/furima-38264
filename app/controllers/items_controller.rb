c190930e6d9ccebbd6294f6d1d9cb30e8d66a727
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-08 15:58:22 +0900

    ActiveStorage導入、アソシエーション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,25 @@ class ItemsController < ApplicationController
   def index
 
   end
+
   def new
-    
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+private
+
+def item_params
+  params.require(:item).permit(:title, :introduction, :price, :category_id, :status_id, :prefecture_id, :delivery_charge_id, :delivery_date_id).merge(user_id: current_user.id)
+end
+
+
 end
