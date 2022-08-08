476a629990e17da546425fd2f37480bd85382760
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-08 17:38:43 +0900

    FactoryBot利用準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    title {Faker::Lorem.sentence}
+    introduction {Faker::Lorem.sentence}
+    price {Faker::Number.between(from:300, to:9999999)}
+    association :user
+    category_id {Faker::Number.between(from:2, to: Category.all.length)}
+    prefecture_id {Faker::Number.between(from:2, to: Prefecture.all.length)}
+    delivery_charge_id {Faker::Number.between(from:2,to: DeliveryCharge.all.length)}
+    delivery_date_id {Faker::Number.between(from:2, to: DeliveryDate.all.length)}
+    status_id {Faker::Number.between(from:2, to:Status.all.length)}
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/internet_nft_art.png'), filename: 'internet_nft_art.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品成功' do
+      it "すべての項目が入力されている" do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品失敗' do
+      it "" do
+      end
+      it "" do
+      end
+      it "" do
+      end
+      it "" do
+      end
+      it "" do
+      end
+      it "" do
+      end
+      it "" do
+      end
+      it "" do
+      end
+      it "" do
+      end
+    end
+  end
+
 end
