10e8d56d1b2ddd872522dca30e500223c8dbcfb7
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-08 15:15:37 +0900

    ActiveBase作成とマイグレーション

--- db/migrate/20220808053843_create_items.rb
+++ db/migrate/20220808053843_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :title, null: false
       t.integer :price, null: false
-      t.text :information, null: false
+      t.text :introduction, null: false
       t.integer :category_id, null: false
       t.integer :status_id, null: false
       t.integer :prefecture_id, null: false
