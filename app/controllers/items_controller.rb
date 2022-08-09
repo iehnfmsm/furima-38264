4a152924613a22092ecdab129cc178cf55a52f1d
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-09 19:39:53 +0900

    商品情報編集機能完成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,15 @@ class ItemsController < ApplicationController
       redirect_to action: :index
     end
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
   
   private
 
