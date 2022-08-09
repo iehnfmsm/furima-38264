96d3454b9903ec4ac12c21859139c83b6fa34a14
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-09 14:18:48 +0900

    ユーザーに紐づくテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,6 +72,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery date can't be blank")
       end
+      it 'ユーザー情報ユーザー情報と紐づいていない時' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end
