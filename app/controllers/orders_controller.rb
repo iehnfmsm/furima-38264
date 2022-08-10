c5e8f80c15d3b53abd085440861d3ccbf7688d2d
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-10 18:05:28 +0900

    ルーティングなどの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,15 @@
 class OrdersController < ApplicationController
+
+  def index
+    @order = Order.new(order_params)
+  end
+
+
+  private
+
+  def order_params
+    params.permit(:zip, :prefecture_id, :shikuchouson, :banchi, :building, :phone_num, :item_id).merge(token: params[:token], user_id: current_user.id)
+  end
+
 end
+
