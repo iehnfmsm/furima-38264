81ed025779b09f1db7dc61113a36099dda7daa1a
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-10 18:19:50 +0900

    購入画面表示まで

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,11 +4,21 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
   end
 
+  def create
+    binding.pry
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
 
   private
 
   def order_params
-    params.permit(:zip, :prefecture_id, :shikuchouson, :banchi, :building, :phone_num, :item_id).merge(token: params[:token], user_id: current_user.id)
+    params.permit(:zip, :prefecture_id, :shikuchouson, :banchi, :building, :phone_num, :item_id).merge(user_id: current_user.id)
   end
 
 end
