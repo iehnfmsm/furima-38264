12aca68ee1992b523d6eb4bb7e7ec79028f63e9c
iehnfmsm <sotet3na700t95@gmail.com>
2022-08-11 17:41:15 +0900

    order_addressの単体テストコード途中

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,13 +1,13 @@
 class OrderAddress
   include ActiveModel::Model
  
-  attr_accessor :zip, :prefecture_id, :shikuchouson, :banchi, :building, :phone_num, :token, :item_id, :user_id
+  attr_accessor :zip, :prefecture_id, :shikuchouson, :banchi, :building, :phone_num, :item_id, :user_id
 
   with_options presence: true do
-    validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "（3桁）-（4桁）で記入してください"}
-    validates :shikuchouson, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :banchi, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
-    validates :phone_num, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "ハイフンなし半角数字で記入してください"}
+    validates :zip, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "（3桁）-（4桁）半角数字で記入してください"}
+    validates :shikuchouson
+    validates :banchi
+    validates :phone_num, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "ハイフンなし半角数字(１０桁又は１１桁)で記入してください"}
     ##validates :token
   end
  
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  before do
+    @order_address = FactoryBot.build(:order_address)
+  end
+
+  describe "" do
+    context "正しく購入できる場合" do
+      it "すべての欄が記入されている" do
+        expect(@order_address).to be_valid
+      end
+      it "建物情報のみ未記入" do
+        @order_address.building = ""
+        expect(@order_address).to be_valid
+      end
+    end
+    context "購入できない場合" do
+      it "郵便番号が未記入" do
+        @order_address.zip = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Zip can't be blank")
+      end
+      it "郵便番号がハイフン無し" do
+        @order_address.zip = "2450002"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Zip （3桁）-（4桁）半角数字で記入してください")
+      end
+      it "郵便番号が全角である" do
+        @order_address.zip = "２４５−０００２"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Zip （3桁）-（4桁）半角数字で記入してください")
+      end
+      it "都道府県を選択していない" do
+        @order_address.prefecture_id = 1
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture 都道府県を選択してください")
+      end
+      it "市区町村が未記入" do
+        @order_address.shikuchouson = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Shikuchouson can't be blank")
+      end
+      it "番地が未記入" do
+        @order_address.banchi = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Banchi can't be blank")
+      end
+      it "電話番号が未記入" do
+        @order_address.phone_num = ""
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone num can't be blank")
+      end
+      it "電話番号がハイフンあり" do
+        @order_address.phone_num = "090-0909-0909"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone num ハイフンなし半角数字(１０桁又は１１桁)で記入してください")
+      end
+      it "電話番号の桁数が多い" do
+        @order_address.phone_num = "090909090909"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone num ハイフンなし半角数字(１０桁又は１１桁)で記入してください")
+      end
+      it "電話番号が全角入力" do
+        @order_address.phone_num = "０９０９０９０９０９０"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone num ハイフンなし半角数字(１０桁又は１１桁)で記入してください")
+      end
+      it "orderと結びついていない" do
+        @order_address.order_id = nil
+        binding.pry
+      end
+      it "" do
+      end
+      it "" do
+      end
+      it "" do
+      end
+      it "" do
+      end
+
+    end
+  end
+end
